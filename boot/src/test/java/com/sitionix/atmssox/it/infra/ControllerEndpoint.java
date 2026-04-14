28d418ec0164cb53c15dd82713f5fe66b4e94e30
vladvinskevitch <vvinskevitch@gmail.com>
2026-04-14 17:26:44 +0300

    [SITIONIX-112] - add patch agent forge-it endpoint

--- boot/src/test/java/com/sitionix/atmssox/it/infra/ControllerEndpoint.java
+++ boot/src/test/java/com/sitionix/atmssox/it/infra/ControllerEndpoint.java
@@ -3,6 +3,7 @@ package com.sitionix.atmssox.it.infra;
 import com.app_afesox.atmssox.api_first.dto.AgentDTO;
 import com.app_afesox.atmssox.api_first.dto.AgentsResponseDTO;
 import com.app_afesox.atmssox.api_first.dto.CreateAgentRequestDTO;
+import com.app_afesox.atmssox.api_first.dto.PatchAgentRequestDTO;
 import com.sitionix.forgeit.domain.endpoint.Endpoint;
 import com.sitionix.forgeit.domain.endpoint.HttpMethod;
 import com.sitionix.forgeit.domain.endpoint.mockmvc.MockmvcDefault;
@@ -42,6 +43,19 @@ public class ControllerEndpoint {
                 AgentDTO.class,
                 (MockmvcDefault) context -> context
                         .header("X-Forge-User-Sub", "1")
+                        .expectStatus(200)
+        );
+    }
+
+    public static Endpoint<PatchAgentRequestDTO, AgentDTO> patchAgent() {
+        return Endpoint.createContract(
+                "/api/v1/agents/{agentId}",
+                HttpMethod.PATCH,
+                PatchAgentRequestDTO.class,
+                AgentDTO.class,
+                (MockmvcDefault) context -> context
+                        .header("X-Forge-User-Sub", "1")
+                        .withRequest("patchAgentRequest.json")
                         .expectStatus(200)
         );
     }
