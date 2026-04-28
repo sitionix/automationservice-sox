3b37b4a1b85214263593b967160b0d722e1775a9
vladvinskevitch <vvinskevitch@gmail.com>
2026-04-28 08:59:05 +0300

    [SITIONIX-123] - extract workspace projection client call executor

--- boot/src/main/java/com/sitionix/atmssox/client/WorkspaceProjectionHttpClient.java
+++ boot/src/main/java/com/sitionix/atmssox/client/WorkspaceProjectionHttpClient.java
@@ -6,11 +6,9 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sitionix.atmssox.domain.client.WorkspaceProjectionClient;
 import com.sitionix.atmssox.domain.exception.AgentValidationException;
-import com.sitionix.atmssox.domain.exception.ClientResponseException;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.HttpStatusCodeException;
 
 @Component
 @RequiredArgsConstructor
@@ -18,21 +16,15 @@ public class WorkspaceProjectionHttpClient implements WorkspaceProjectionClient 
 
     private final SiteApi siteApi;
     private final ObjectMapper objectMapper;
+    private final WorkspaceProjectionClientCallExecutor clientCallExecutor;
 
     @Override
     public JsonNode getSiteOverview(final Long userId, final UUID siteId) {
         this.validateUserId(userId);
-        try {
+        return this.clientCallExecutor.execute(() -> {
             final SiteOverviewDTO response = this.siteApi.getSiteOverview(siteId);
             return this.objectMapper.valueToTree(response);
-        } catch (HttpStatusCodeException exception) {
-            throw new ClientResponseException(
-                    exception.getStatusCode().value(),
-                    exception.getResponseBodyAsString(),
-                    exception.getResponseHeaders() == null ? null : exception.getResponseHeaders(),
-                    exception
-            );
-        }
+        });
     }
 
     private void validateUserId(final Long userId) {
