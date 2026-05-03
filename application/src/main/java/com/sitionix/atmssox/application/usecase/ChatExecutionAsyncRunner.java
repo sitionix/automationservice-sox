636c7e73e0862bb7639f83c3b333e4d1bf5b01e5
vladvinskevitch <vvinskevitch@gmail.com>
2026-05-03 10:09:37 +0300

    [SITIONIX-127] - remove async processor cycle with provider

--- application/src/main/java/com/sitionix/atmssox/application/usecase/ChatExecutionAsyncRunner.java
+++ application/src/main/java/com/sitionix/atmssox/application/usecase/ChatExecutionAsyncRunner.java
@@ -3,6 +3,7 @@ package com.sitionix.atmssox.application.usecase;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,12 +13,12 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ChatExecutionAsyncRunner {
 
-    private final ChatExecutionAsyncProcessor chatExecutionAsyncProcessor;
+    private final ObjectProvider<ChatExecutionAsyncProcessor> chatExecutionAsyncProcessorProvider;
 
     @Async("contextOptimizerTaskExecutor")
     @Transactional
     public void processAsync(final UUID executionId) {
         log.info("[CHAT_EXECUTION] async started executionId={}", executionId);
-        this.chatExecutionAsyncProcessor.process(executionId);
+        this.chatExecutionAsyncProcessorProvider.getObject().process(executionId);
     }
 }
