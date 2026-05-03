349e5e4b137f4c7785f0dfc96c148c02ba4c7f43
vladvinskevitch <vvinskevitch@gmail.com>
2026-05-03 15:04:01 +0300

    [SITIONIX-127] - bump version

--- db-migration/V14__add_input_message_id_to_chat_executions.sql
+++ db-migration/V14__add_input_message_id_to_chat_executions.sql
@@ -1,20 +1,6 @@
 ALTER TABLE chat_executions
     ADD COLUMN input_message_id UUID;
 
-INSERT INTO conversation_messages (message_id, conversation_id, author_type, author_id, content, created_at)
-SELECT ce.execution_id,
-       ce.conversation_id,
-       'USER',
-       ce.user_id::text,
-       ce.request_message,
-       COALESCE(ce.started_at, ce.created_at)
-FROM chat_executions ce
-WHERE ce.input_message_id IS NULL;
-
-UPDATE chat_executions ce
-SET input_message_id = ce.execution_id
-WHERE ce.input_message_id IS NULL;
-
 ALTER TABLE chat_executions
     ALTER COLUMN input_message_id SET NOT NULL;
 
