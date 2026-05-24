aa5881840ee42c1e9af81def4d8faf495555afee
vladvinskevitch <vvinskevitch@gmail.com>
2026-05-24 14:32:55 +0300

    [SITIONIX-137] - restore async processor mock in forge IT flows

--- boot/src/test/java/com/sitionix/atmssox/it/ChatExecutionRetrievalFlowIT.java
+++ boot/src/test/java/com/sitionix/atmssox/it/ChatExecutionRetrievalFlowIT.java
@@ -1,6 +1,7 @@
 package com.sitionix.atmssox.it;
 
 import com.sitionix.atmssox.domain.client.OpenAiChatClient;
+import com.sitionix.atmssox.application.usecase.ChatExecutionAsyncProcessor;
 import com.sitionix.atmssox.it.infra.ControllerEndpoint;
 import com.sitionix.atmssox.it.infra.TestManager;
 import com.sitionix.atmssox.postgresql.entity.agent.AgentEntity;
@@ -37,6 +38,9 @@ class ChatExecutionRetrievalFlowIT {
 
     @MockBean
     private OpenAiChatClient openAiChatClient;
+
+    @MockBean
+    private ChatExecutionAsyncProcessor chatExecutionAsyncProcessor;
 
     @Test
     @DisplayName("Should submit chat by executions path and persist execution")
